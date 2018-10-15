03468d5e0a93bcdaa19eaf54be1cc4625e3e7665
aben20807 <aben20807@gmail.com>
2018-10-15 23:52:34 +0800

    Not use mask, add crc8 from linux

--- week01/crc.c
+++ week01/crc.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdint.h>
 
 void print_bits(size_t const size, void const *const ptr)
 {
@@ -13,16 +14,40 @@ void print_bits(size_t const size, void const *const ptr)
 
 unsigned char get_crc(unsigned char c)
 {
-    unsigned int mask = (1 << 15);
     unsigned int poly = (0b100000111 << 7);
     unsigned int crc = (c << 8);
-    for (int j = 0; j < 8; j++) {
-        if (crc & mask) {
+    for (int i = 0; i < 8; i++) {
+        if ((crc >> (16 - i - 1)) & 1) {
             crc ^= poly;
         }
-        mask >>= 1;
         poly >>= 1;
     }
+    return crc;
+}
+
+#define CRC8_TABLE_SIZE 256
+typedef uint8_t u8;
+typedef uint16_t u16;
+void crc8_populate_msb(u8 table[CRC8_TABLE_SIZE], u16 polynomial)
+{
+    printf("%d\n", polynomial);
+    int i, j;
+    const u8 msbit = 0x80;
+    u8 t = msbit;
+
+    table[0] = 0;
+
+    for (i = 1; i < CRC8_TABLE_SIZE; i *= 2) {
+        t = (t << 1) ^ (t & msbit ? polynomial : 0);
+        for (j = 0; j < i; j++)
+            table[i+j] = table[j] ^ t;
+    }
+}
+
+u8 crc8(const u8 table[CRC8_TABLE_SIZE], u8 pdata, u8 crc)
+{
+    crc = table[(crc ^ pdata) & 0xff];
+    // printf("%d, %d\n", 0 ^ pdata, table[87]);
     return crc;
 }
 
@@ -31,5 +56,16 @@ int main(){
     print_bits(sizeof(c), &c);
     unsigned char crc = get_crc(c);
     print_bits(sizeof(crc), &crc);
+
+    unsigned char table[CRC8_TABLE_SIZE];
+    crc8_populate_msb(table, 0b100000111);
+    for (int i = 0; i < CRC8_TABLE_SIZE; i++) {
+        printf("%4d", table[i]);
+        if ((i + 1) % 16 == 0)
+            puts("");
+    }
+    puts("");
+    u8 crc2 = crc8(table, c, 0);
+    print_bits(sizeof(crc2), &crc2);
     return 0;
 }
